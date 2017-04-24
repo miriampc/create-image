a2d53a5f9fc8e1daec7e328506cb2637d45ed53e
Miriam Peralta <miiriampc@gmail.com>
2017-04-24 13:12:37 -0500

    ejerciio guiado desarrollado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
+window.addEventListener('load',function(){
+  function creandoDiv(getURL){
+    var contenedor=document.getElementById('contenedor');
+    var div= document.createElement('div');
+    var img = document.createElement('img');
 
+    img.setAttribute("src",getURL);
+    div.classList.add("contenedor-imagen");
+    div.appendChild(img);
+    contenedor.appendChild(div);
+  }
+
+  var inputURL=document.getElementById('url');
+
+  inputURL.onkeydown = function getURL(e){
+    if(e.keyCode == 13){
+      creandoDiv(this.value);
+      this.value="";
+    }
+  }
+  //inputURL.onkeydown=getURL;
+
+});
